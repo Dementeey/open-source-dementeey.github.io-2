bb67d89036d1bb53908ed632243525e3d67885cb
dementeeyMac <metallist9656@gmail.com>
2018-07-26 16:19:14 +0300

    Закончил с сеткой и модальным окном

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,22 @@
 // vars
 const html = document.documentElement;
 const aboutMeBtn = document.querySelector('.about-me');
+const aboutCloseBtn = document.querySelector('.modal__close-btn');
 const modal = document.querySelector('.modal');
+const modalOverlay = document.querySelector('.modal-overlay');
 
 
 // functions
 const aboutMeToggle = () => {
-  return modal;
+  if ( !modal.classList.contains('is-close') ) {
+    modal.classList.add('is-close');
+    modalOverlay.classList.add('is-close');
+    return
+  }
+
+  modalOverlay.classList.remove('is-close');
+  modal.classList.remove('is-close');
+  return
 };
 
 
@@ -17,7 +27,16 @@ document.addEventListener('DOMContentLoaded', () => {
     return html.classList.add('touch');
   }
 
-  return html.classList.add('no-touch');
+  html.classList.add('no-touch');
+  return
 })
-
 aboutMeBtn.addEventListener('click', aboutMeToggle);
+aboutCloseBtn.addEventListener('click', aboutMeToggle);
+modalOverlay.addEventListener('click', aboutMeToggle);
+document.addEventListener('keydown', (e) => {
+  const keyCode = e.keyCode || e.which;
+  if (keyCode === 27) {
+    e.preventDefault();
+    aboutMeToggle();
+  }
+})
